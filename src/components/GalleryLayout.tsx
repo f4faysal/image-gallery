396742e03ade1f548fa7fe33b493c4b2bbb765c9
f4faysal <f4faysals@gmail.com>
2023-11-01 15:20:11 +0600

    Import all image

--- src/components/GalleryLayout.tsx
+++ src/components/GalleryLayout.tsx
@@ -1,7 +1,18 @@
+import { defaultImage } from "../constant/global";
+
 const GalleryLayout = () => {
   return (
     <div>
       <h1>GalleryLayout</h1>
+
+      {defaultImage.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.image} alt={item.alt} />
+            <p>{item.alt}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/constant/global.ts
@@ -0,0 +1,57 @@
+export const defaultImage = [
+  {
+    id: "1",
+    image: "/images/image-1.webp",
+    alt: "image-1",
+  },
+  {
+    id: "2",
+    image: "/images/image-2.webp",
+    alt: "image-2",
+  },
+  {
+    id: "3",
+    image: "/images/image-3.webp",
+    alt: "image-3",
+  },
+  {
+    id: "4",
+    image: "/images/image-4.webp",
+    alt: "image-4",
+  },
+  {
+    id: "5",
+    image: "/images/image-5.webp",
+    alt: "image-5",
+  },
+  {
+    id: "6",
+    image: "/images/image-6.webp",
+    alt: "image-6",
+  },
+  {
+    id: "7",
+    image: "/images/image-7.webp",
+    alt: "image-7",
+  },
+  {
+    id: "8",
+    image: "/images/image-8.webp",
+    alt: "image-8",
+  },
+  {
+    id: "9",
+    image: "/images/image-9.webp",
+    alt: "image-9",
+  },
+  {
+    id: "10",
+    image: "/images/image-10.jpeg",
+    alt: "image-10",
+  },
+  {
+    id: "11",
+    image: "/images/image-11.jpeg",
+    alt: "image-11",
+  },
+];
